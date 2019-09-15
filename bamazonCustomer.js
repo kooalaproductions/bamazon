4ec23cc19f57341639265ec6d0deae2900ad6e42
Sky <erniesanchez619@gmial.com>
2019-09-14 20:49:53 -0700

    created bamazonCustomer.js and successfully updated mySQL

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,8 +1,8 @@
-// let keys = require("./pass.js");
-let mysql = require('mysql');
-let inquirer = require('inquirer');
+//npm i
+var mysql = require("mysql");
+var inquirer = require("inquirer");
 
-let connection = mysql.createConnection({
+var connection = mysql.createConnection({
     host: "localhost",
 
     // Your port; if not 3306
@@ -13,127 +13,79 @@ let connection = mysql.createConnection({
 
     // Your password
     password: "password",
-    // database: "greatBay_DB"
+
     database: "bamazon"
 });
 
 connection.connect(function (err) {
     if (err) throw err;
-    // console.log("connected as id " + connection.threadId + "\n");
+    productPost();
+
 });
 
-function exit() {
-    connection.end();
-    console.log("Thank you for visiting GreatBay!");
-}
+function productPost() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        console.log("********BAMAZON************");
+        console.log("-----------------------");
+        for (var i = 0; i < res.length; i++) { //prints out all the items for mysql
+            console.log('Item Number: ' + res[i].item_id + '\nName: ' + res[i].itemName + '\nDepartment: ' + res[i].department_name + '\nPrice: $' + res[i].price + '\nQuantity: ' + res[i].stock_quantity + '\n----------------------------------------')
 
-function bid() {
-    connection.query('SELECT * FROM auctions', (err, res) => {
-        if (err) throw err;
-        let itemForSale = [];
-        let idForSale = [];
-        res.forEach(function (i) {
-            console.log("ID: " + i.id + " | " + "Item: " + i.item_name + " | " + "Current Bid: " + i.highest_bid);
-            console.log("-----------------------------------");
-            itemForSale.push(i.item_name);
-            idForSale.push(i);
-        });
-        inquirer.prompt([{
-            name: "choice",
-            type: 'list',
-            message: "What would you like to bid on?",
-            choices: itemForSale
-        }, {
-            type: "input",
-            name: "bid",
-            message: "How much do you want to bid?"
-        }]).then(answers => {
-            let index = itemForSale.indexOf(answers.choice);
-            parseInt(answers.bid) > parseInt(idForSale[index].highest_bid) ? (console.log("You are the highest bidder"), updatePrice(answers.bid, answers.choice)) : console.log("You didn't bid high enough");
-        });
+        };
+        selectItem();
     });
-}
+};
 
-function updatePrice(bid,name){
-    connection.query("UPDATE auctions SET ? WHERE ?",[{
-        highest_bid: bid
-    },{
-        item_name:name
-    }],function(err, res) {
-        if (err) throw err;
-        console.log(res.affectedRows + " products updated!\n");
-        // Call deleteProduct AFTER the UPDATE completes
-        mainMenu();
-      }
-    );
-}
-
-function postItem() {
-    inquirer
-        .prompt([{
-            type: "input",
-            message: "What is your item called?",
-            name: "itemName"
-        },
-        {
-            type: "input",
-            message: "What category does it fall in?",
-            name: "categoryName"
-        },
-        {
-            type: "input",
-            message: "How much is your item?",
-            name: "startingBid"
-        }
-        ])
-        .then(answer => {
-            connection.query("INSERT INTO auctions SET ?",
-                {
-                    item_name: answer.itemName,
-                    category: answer.categoryName,
-                    starting_bid: answer.startingBid,
-                    highest_bid: answer.startingBid
-                },
-                function (err, res) {
-                    if (err) throw err;
-                    console.log("Success!!");
-                    mainMenu();
-                });
-        });
-}
-
-function mainMenu() {
+function selectItem() { //inquirer
     inquirer.prompt([{
-        type: "input",
-        name: "username",
-        message: "Please enter your username."
-    },
-    {
-        type: "password",
-        name: "password",
-        message: "Please enter your password."
-    }, {
-        type: "list",
-        choices: ["POST", "BID", "EXIT"],
-        message: "What would you like to do?",
-        name: 'choice'
-    }]).then(answers => {
-        switch (answers.choice) {
-            case 'POST':
-                postItem();
-                break;
-            case 'BID':
-                bid();
-                break;
-            case 'EXIT':
-                exit();
-                break;
-            default:
-                console.log("??");
+            name: "itemNum",
+            type: "input",
+            message: "Enter the item number you would like to purchase: ",
+        },
+        {
+            name: "itemQuan",
+            type: "input",
+            message: "How many would you like to purchase: "
         }
-    });
-}
+    ]).then(function (itemPicked) {
+        connection.query("SELECT * FROM products WHERE item_id = ?", [itemPicked.itemNum], function (err, res) { //will go to the item number entered
+            if (err) throw err;
+            for (var i = 0; i < res.length; i++) {
+                var numQuan = parseInt(itemPicked.itemQuan);
+                var total = itemPicked.itemQuan * res[i].price;
+                var itemName = res[i].itemName;
+                var itemUpdate = res[i].stock_quantity - numQuan;
+                if (numQuan > res[i].stock_quantity) {//will print out if not enough in stock
+                    console.log("Insufficient quantity!");
+                    connection.end();
+                } else {//continue purchase
+
+                    console.log("Item chosen: " + itemName);
+                    sqlUpdate(itemPicked.itemNum, itemUpdate);
+                    console.log("There is " + itemUpdate + " " + itemName + " left");
+                }
+
+                function sqlUpdate(target_item, itemUpdate) {
+
+                    connection.query("UPDATE products SET ? WHERE ?",
+                        [{
+                                stock_quantity: itemUpdate
+                            },
+                            {
+                                item_id: target_item
+                            }
+                        ],
+                        function (err, res) {
+                            if (err) throw err;
+                            console.log("Inventory updated");
+                            connection.end();
+                            console.log("Total purchase $" + total)
+
+                        });
+                }
+            }
 
 
-//When app first loads call mainMenu
-mainMenu();
+        });
+    })
+
+};
